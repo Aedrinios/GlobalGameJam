dafec4754bc958da48a61a0446c8af99cf285a6b
Aedrinios <clementclerc2@gmail.com>
2020-02-01 00:25:59 +0100

    work in progress atelier scene

---
+++ GGJ2020/Assets/_Scripts/DragObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private Rigidbody2D rb2D;
+
+    private Vector3 convertMousePosition;
+    private Vector2 screenMousePosition;
+    private Vector2 mouseDirection;
+    private Vector2 convertTransform;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseDown()
+    {
+        convertMousePosition = Input.mousePosition;
+        convertMousePosition.z = 10;
+        convertMousePosition = mainCamera.ScreenToWorldPoint(convertMousePosition);
+        screenMousePosition = convertMousePosition;
+        FollowMouse();
+    }
+
+    private void FollowMouse()
+    {
+        convertTransform = transform.position;
+        mouseDirection = (screenMousePosition - convertTransform).normalized;
+        rb2D.AddForce(mouseDirection * Time.fixedDeltaTime);
+    }
+}
