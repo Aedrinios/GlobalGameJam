bb64ca04faaeb9ef14045306e9f65e138b4a58a9
Blot Paul <blot.paulluap@sfr.fr>
2020-02-01 00:24:33 +0100

    Il va faire tout noir

---
+++ GGJ2020/Assets/Scripts/SpawnerSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerSystem : MonoBehaviour
+{
+    public GameObject monsterSpawn; 
+
+    public KeyCode SpawnButton;
+    public Enum.Side sideSpawn = Enum.Side.Left;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(SpawnButton))
+        {
+            SpawnNewMonster(); 
+        }
+    }
+
+    public void SpawnNewMonster()
+    {
+        GameObject newMonster = Instantiate(monsterSpawn, transform.position, transform.rotation) as GameObject;
+        newMonster.GetComponent<MonsterSystem>().side = sideSpawn; 
+    }
+}
