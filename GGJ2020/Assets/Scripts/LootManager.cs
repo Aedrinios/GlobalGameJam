57f935bfabccd5a754b57dc8b94fb9a01988bd80
Blot Paul <blot.paulluap@sfr.fr>
2020-02-01 20:40:12 +0100

    C'est tres la progression

---
+++ GGJ2020/Assets/Scripts/LootManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    public float delayLoot = 5;
+    public List<PartBody> stockPart_P1 = new List<PartBody>();
+    public List<PartBody> stockPart_P2 = new List<PartBody>();
+
+    float chrono = 0;
+
+
+    private void Update()
+    {
+        chrono += Time.deltaTime; 
+        if (chrono >= delayLoot)
+        {
+            //CleanLoot();
+            chrono = 0;
+        }
+    }
+
+    public void CleanLoot()
+    {
+        GameObject[] pickUp = GameObject.FindGameObjectsWithTag("PickUp");
+
+        for (int i = 0; i < pickUp.Length; i++)
+        {
+            PartSystem partSyst = pickUp[i].GetComponent<PartSystem>();
+
+            stockPart_P1.Add(partSyst.partBody);
+            Destroy(pickUp[i]);
+        }
+    } 
+
+}
