b2d1593351acd79f70d9c08d3d77d897f2c958e6
Blot Paul <blot.paulluap@sfr.fr>
2020-02-01 02:58:51 +0100

    ca push

---
+++ GGJ2020/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] sound;
+    [Range(0,1)] public float volume;
+
+    AudioSource source;
+    private void Start()
+    {
+        source = GetComponent<AudioSource>(); 
+    }
+
+    public void PlaySound()
+    {
+        source.volume = volume; 
+        int i = Random.Range(0, sound.Length);
+        source.PlayOneShot(sound[i]); 
+    }
+}
