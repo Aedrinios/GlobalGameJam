add64bde92c24d4c79495eb16a00588b76536ab3
Blot Paul <blot.paulluap@sfr.fr>
2020-02-01 01:24:23 +0100

    Les parties en partie

---
+++ GGJ2020/Assets/Scripts/MonsterStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterStats : MonoBehaviour
+{
+    public int strength = 10;
+    public int constitution = 10;
+    public int agility = 10;
+
+    PartSystem[] allPartSystem; 
+
+    private void Awake()
+    {
+        allPartSystem = GetComponentsInChildren<PartSystem>();
+
+        for (int i = 0; i < allPartSystem.Length; i++)
+        {
+            strength += allPartSystem[i].bonusStrength;
+            constitution += allPartSystem[i].bonusConstitution;
+            agility += allPartSystem[i].bonusAgility;
+        }
+
+    }
+
+
+}
