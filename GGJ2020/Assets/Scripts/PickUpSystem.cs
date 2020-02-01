e6c176c40231e7d03b912147988701e42d56f3ae
Blot Paul <blot.paulluap@sfr.fr>
2020-02-01 03:37:46 +0100

    C pa vomi

---
+++ GGJ2020/Assets/Scripts/PickUpSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; 
+
+public class PickUpSystem : MonoBehaviour
+{
+    public UnityEvent Gibs;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "PickUp")
+        {
+            Gibs.Invoke(); 
+        }
+    }
+}
